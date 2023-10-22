fa99670048405b2f24aa28a3a9e38575c5bc06a9
sujal3011 <jaiswalsujal639@gmail.com>
2023-10-23 00:56:37 +0530

    fixed bug (searching)

--- src/Components/Dashboard/CandidateList/CandidateList.jsx
+++ src/Components/Dashboard/CandidateList/CandidateList.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 import CandidateCard from "./CandidateCard";
 import {
   onValue,
@@ -12,30 +14,52 @@ import SearchBar from "../SearchBar";
 
 const CandidateList = () => {
   const [candidates, setCandidates] = useState([]);
-  const [searchQuery, setSearchQuery] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
+
+
+  const showSearchMessage = (found) => {
+      if(found){
+        toast.success('candidate found !', {
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: 1000,
+            theme: "colored",
+            // className: 'search-success'
+        });
+      }
+      else{
+        toast.error('No candidate found !', {
+          position: toast.POSITION.TOP_RIGHT,
+          autoClose: 1000,
+          theme: "colored",
+          // className: 'search-failure'
+      });
+    }
+};
 
   useEffect(() => {
-    const query = ref(realTimeDB, "candidates");
-    return onValue(query, (snapshot) => {
-      const data = snapshot.val();
-      if (snapshot.exists()) {
-        var list = [];
-        Object.values(data).map((project, idx) => {
-          project.id = Object.keys(data)[idx];
-          list.push(project);
-        });
-        console.log(list);
-        setCandidates(list);
-      }
-    });
-  }, []);
+    if(searchQuery===""){
+      const query = ref(realTimeDB, "candidates");
+      return onValue(query, (snapshot) => {
+        const data = snapshot.val();
+        if (snapshot.exists()) {
+          var list = [];
+          Object.values(data).map((project, idx) => {
+            project.id = Object.keys(data)[idx];
+            list.push(project);
+          });
+          console.log(list);
+          setCandidates(list);
+        }
+      });
+    }
+  }, [searchQuery]);
 
   const isInteger = (str) => {
-    const num = parseInt(str);
-    return !isNaN(num) && Number.isInteger(num);
+    return /^\d+$/.test(str);
   };
 
   const handleSearch = () => {
+    if(searchQuery==="") return;
     const candidatesRef = ref(realTimeDB, "candidates");
     const queryName = query(
       candidatesRef,
@@ -47,6 +71,8 @@ const CandidateList = () => {
       const data = snapshot.val();
       if (snapshot.exists()) {
         queryFound = true;
+        showSearchMessage(queryFound);
+        // setSearchQuery("");
         var list = [];
         Object.values(data).map((project, idx) => {
           project.id = Object.keys(data)[idx];
@@ -61,12 +87,14 @@ const CandidateList = () => {
       const queryRoll = query(
         candidatesRef,
         orderByChild("rollNumber"),
-        equalTo(searchQuery)
+        equalTo(searchQuery.toUpperCase())
       );
       onValue(queryRoll, (snapshot) => {
         const data = snapshot.val();
         if (snapshot.exists()) {
           queryFound = true;
+          showSearchMessage(queryFound);
+          // setSearchQuery("");
           var list = [];
           Object.values(data).map((project, idx) => {
             project.id = Object.keys(data)[idx];
@@ -90,6 +118,8 @@ const CandidateList = () => {
         if (snapshot.exists()) {
           console.log(snapshot.val());
           queryFound = true;
+          showSearchMessage(queryFound);
+          // setSearchQuery("");
           var list = [];
           Object.values(data).map((project, idx) => {
             project.id = Object.keys(data)[idx];
@@ -100,8 +130,12 @@ const CandidateList = () => {
         }
       });
     }
-
-    if (!queryFound) setCandidates([]);
+    if (!queryFound){
+      setCandidates([]);
+      showSearchMessage(queryFound);
+      // setSearchQuery("");
+    }
+    
   };
 
   return (
@@ -109,7 +143,7 @@ const CandidateList = () => {
       <div className="font-head font-semibold text-5xl text-onSurface flex justify-start">
         Candidate List
       </div>
-      <SearchBar setSearchQuery={setSearchQuery} handleSearch={handleSearch} />
+      <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} handleSearch={handleSearch} />
       <div className="w-full mt-16">
         {candidates.map((candidate, index) => {
           return (
@@ -121,6 +155,7 @@ const CandidateList = () => {
           );
         })}
       </div>
+      <ToastContainer />
     </div>
   );
 };
--- src/Components/Dashboard/SearchBar.jsx
+++ src/Components/Dashboard/SearchBar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-const SearchBar = ({setSearchQuery,handleSearch }) => {
+const SearchBar = ({searchQuery,setSearchQuery,handleSearch }) => {
+
   return (
     <div className="mb-3 mt-3">
       <div className=" mb-4 flex w-full flex-wrap items-stretch w-4/5">
        
         <input
           type="search"
-          className="block w-4/5 rounded-md border-0 outline-none bg-transparent py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-onSurface focus:ring-2 focus:ring-inset focus:text-onSurface sm:text-sm sm:leading-6"
+          className="block w-4/5 rounded-md border-0 outline-none bg-transparent py-1.5 pl-7 pr-20 text-onSurface ring-1 ring-inset ring-gray-300 placeholder:text-onSurface focus:ring-2 focus:ring-inset focus:text-onSurface sm:text-sm sm:leading-6" value={searchQuery}
           placeholder="Search" onChange={(e)=>{setSearchQuery(e.target.value)}}
         />
 
