6911fb9dc4409d4000f9eb81a609fb817b6208d7
Sivam2313 <sivamdas7@gmail.com>
2023-10-25 02:34:51 +0530

    added domain section in form

--- src/Components/Form/Stepper/Elements/Message.jsx
+++ src/Components/Form/Stepper/Elements/Message.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className='text-onSurface2 font-head text-2xl mt-18'>Only Applicable for those who have filled for Web/App development or Graphics Design</div>
+    <div className='text-onSurface2 font-head text-xl pt-24'>Only Applicable for those who have filled for Web/App development or Graphics Design</div>
   )
 }
 
