db4bcdacd1896378e04e9b4d30a0a2abbe9c23df
Sivam2313 <sivamdas7@gmail.com>
2023-12-18 13:11:29 +0530

    page flip animation

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,6 +7,7 @@ import { motion } from "framer-motion";
 import { animationOne, transition } from "../../Animations";
 import ArrowLeft from "../Icons/ArrowLeft";
 import Roles from "./Roles";
+import { set } from "lodash";
 
 
 const Home = () => {
@@ -26,7 +27,7 @@ const Home = () => {
         setTimeout(typeWriter, speed);
       }
     }
-    typeWriter();
+    setTimeout(typeWriter, 1000);
   
   }, []);
 
@@ -40,21 +41,32 @@ const Home = () => {
       exit='out'
       variants={animationOne}
       transition={transition}
+      className="snap-mandatory snap-y overflow-y-scroll overflow-x-hidden snap-strict h-screen w-screen fixed flex flex-col"
     >
       <div 
         style={{ backgroundImage: `url(${homepageGif})` }}
-        className="bg-cover bg-center h-screen w-screen flex flex-col  z-10">
+        className="bg-cover bg-center h-screen w-screen flex flex-col snap-start z-10">
         <Navbar />
         <div className="w-screen h-screen flex flex-col justify-center items-center">
-          <div className="font-title text-onSecondary text-9xl pb-12">
+          <motion.div 
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            transition={{delay:0.5, duration:2}}
+            className="font-title text-onSecondary text-9xl pb-12"
+          >
             WELCOME TO THE
-          </div>
+          </motion.div>
           <div className="font-title text-primary text-9xl">
             <span ref={textAnimation}></span>
             <motion.div className="inline" initial={{opacity:0}} animate={{opacity:1}} transition={{ repeat: "repeat", repeatDelay:0.2, duration: 0.2 }}>_</motion.div>
           </div>
 
-          <div className="flex pt-12">
+          <motion.div 
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            transition={{delay:2.5, duration:2}}
+            className="flex pt-12"
+          >
             <div className="drop-shadow-glow text-primary">
               <svg width="37" height="32" viewBox="0 0 50 42" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M12.25 7.21L28.6667 21L12.25 34.79L16.6667 38.5L37.5 21L16.6667 3.5L12.25 7.21Z" fill="white"/>
@@ -68,10 +80,12 @@ const Home = () => {
               <path d="M35.0234 5.3098C33.9821 4.4523 32.3034 4.4523 31.2621 5.3098L13.6034 19.8523C12.7746 20.5348 12.7746 21.6373 13.6034 22.3198L31.2621 36.8623C32.3034 37.7198 33.9821 37.7198 35.0234 36.8623C36.0646 36.0048 36.0646 34.6223 35.0234 33.7648L19.6384 21.0773L35.0446 8.3898C36.0646 7.5498 36.0646 6.1498 35.0234 5.3098Z" fill="white"/>
               </svg>
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
-      <Roles />
+      <div className="snap-start h-fit">
+        <Roles />
+      </div>
     </motion.div>
   );
 };
--- src/Components/Home/RolesCard.jsx
+++ src/Components/Home/RolesCard.jsx
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import logo from '../../Assets/logoInverted.png'
-import { motion } from 'framer-motion';
+import { inView, motion, useInView } from 'framer-motion';
+import { useState } from 'react';
+import "./Styles/RolesCard.css"
 
-const RolesCard = ({label}) => {
+const RolesCard = ({label,timeout}) => {
+  const [isAnimating, setIsAnimating] = useState(false)
+  const [isFlipped, setIsFlipped] = useState(false)
+  const flipCard = useRef(null)
+  const inView = useInView(flipCard)
+
+  function handleflip(){
+    if(!isAnimating){
+      setIsFlipped(!isFlipped)
+      setIsAnimating(true)
+    }
+  }
+
+  useEffect(()=>{
+    if(inView){
+      setTimeout(()=>{
+        setIsFlipped(true)
+      },timeout*1000)
+    }
+  },[inView])
+
   return (
-    <motion.div layout className='w-2/12 h-2/3 min-h-[400px] min-w-[100px] bg-transparent rounded-2xl border-[#E6C449] border-2 flex flex-col items-center justify-center hover:drop-shadow-glow'>
-        <div className='w-[2px] h-full  bg-primary'></div>
-        <div className='rounded-full border-[#E6C449] border-2 w-[150px] h-[150px] absolute mt-3 bg-black flex justify-center items-center'>
-            <img src={logo} className='w-10/12' />
-        </div>
-    </motion.div>
+    <div className='flip-card' ref={flipCard} onClick={handleflip}>
+      <motion.div 
+        initial={false}
+        animate={{ rotateY: isFlipped ? 180 : 360, x: isFlipped ? 100 : -100 }}
+        transition={{ duration: 0.2, aniationDirection: 'normal'}}
+        onAnimationComplete={()=>setIsAnimating(false)}
+        className='flip-card-inner w-full h-full'
+      >
+          <div className='w-full h-full min-h-[400px] min-w-[200px] bg-transparent rounded-2xl border-[#E6C449] border-2 flex flex-col items-center justify-center flip-card-front'>
+            <div className='w-[2px] h-full  bg-primary'></div>
+            <div className='rounded-full border-[#E6C449] border-2 w-[150px] h-[150px] absolute mt-3 bg-black flex justify-center items-center'>
+                <img src={logo} className='w-10/12' />
+            </div>
+          </div>
+          <div className='w-full h-full min-h-[400px] min-w-[200px] bg-black rounded-2xl border-[#E6C449] border-2 flex flex-col items-center justify-center flip-card-back'>
+            <div className='font-title text-xl text-onSecondary'>
+              {label}
+            </div>
+          </div>
+      </motion.div>
+    </div>
   )
 }
 
