088258aa39224d887b4b6daae617d4803060f190
Sivam2313 <sivamdas7@gmail.com>
2025-01-12 11:13:52 +0530

    imail fix

--- src/Components/Form/Buttons.jsx
+++ src/Components/Form/Buttons.jsx
@@ -58,7 +58,7 @@ const Buttons = ({active,setActive,size,submitHandler, name, roll, branch, pmail
       var [registration, domain] = imail.split("@");
       registration = registration.trim().toLowerCase();
       const isValidDomain = domain === "btech.nitdgp.ac.in";
-      if(imail.trim()==="" || !validator.isEmail(imail.trim()) || !isValidDomain || registration.length!==11 || registration[registration.length-6]!=='u' || registration[registration.length-7]!=='3' || registration[registration.length-8]!=='2')
+      if(imail.trim()==="" || !validator.isEmail(imail.trim()) || !isValidDomain || (registration.length!==11 && registration.length!==12) || registration[registration.length-6]!=='u' || registration[registration.length-7]!=='3' || registration[registration.length-8]!=='2')
       {
         setIsValidimail(false);
         return;
